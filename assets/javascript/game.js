6a33b2cbba8dbf4f32053037a943fcfb8106224c
Jessica Elias <jm.elias1990@yahoo.com>
2019-02-07 20:42:57 -0500

    Psychic-Game

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+
+var winCount = 1;
+var lossCount = 0;
+var guessesRemaining = 10;
+var guessedLetters = [];
+
+var alphabet =
+    ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
+    ,"r","s","t","u","v","w","x","y","z"];
+
+
+var randomIndex = Math.floor(Math.random() * alphabet.length);
+var computerChoice = alphabet[randomIndex];
+
+console.log(computerChoice);
+
+
+document.onkeyup = function(event) {
+  var userChoice = event.key;
+
+  var regexp = /[a-z]/gi;
+    if (!regexp.test(userChoice)) {
+      alert("please enter a letter");
+    }
+    else {
+      console.log(userChoice);
+    }
+
+    if (guessesRemaining <= 0) {
+      lossCount++;
+      document.getElementById("lossCount").innerHTML = lossCount++;
+      console.log("You lost!");
+      alert("You lost!");
+      guessesRemaining = 10;
+      guessedLetters = [];
+      document.getElementById("guessedLetters").innerHTML = guessedLetters;
+      document.getElementById("guessesRemaining").innerHTML = 10;
+      randomIndex = Math.floor(Math.random() * alphabet.length);
+      computerChoice = alphabet[randomIndex];
+      console.log(computerChoice);
+    }
+
+    if (computerChoice === userChoice) {
+      console.log("You won!");
+      alert("You won!");
+      document.getElementById("winCount").innerHTML = winCount++;
+      guessedLetters = [];
+      document.getElementById("guessedLetters").innerHTML = guessedLetters;
+      randomIndex = Math.floor(Math.random() * alphabet.length);
+      computerChoice = alphabet[randomIndex];
+      console.log(computerChoice);
+      guessesRemaining = 10;
+      document.getElementById("guessesRemaining").innerHTML = 10;
+    } else {
+      console.log("Guess again!");
+      document.getElementById("guessesRemaining").innerHTML = guessesRemaining--;
+      guessedLetters.push(userChoice);
+      document.getElementById("guessedLetters").innerHTML = guessedLetters;
+    }
+}
